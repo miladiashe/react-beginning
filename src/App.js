7f75925521a6e1cd46f8d06738721bce81c391c4
miladiashe <dkdud0122@naver.com>
2022-01-21 04:41:59 +0900

    다 지우고 다시

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
-
-function Hello() {
-    useEffect(() => {
-        console.log("만들어졌다");
-        return () => {
-            console.log("사라졌다");
-        };
-    }, []);
-
-    return <h1>hello</h1>;
-}
-
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
-    return (
-        <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-        </div>
-    );
+    return <div></div>;
 }
 
 export default App;
