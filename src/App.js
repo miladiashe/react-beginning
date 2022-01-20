e2c2eaf2ec2e0524bc508bc641605c49aa2a6d4b
miladiashe <dkdud0122@naver.com>
2022-01-21 01:56:33 +0900

    많은것을...배웠습니다...
    저도 이렇게 커밋 메시지 쓰면 안되는거 알아요

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+    const [value, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onChange = (event) => setKeyword(event.target.value);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log("언제나 실행되는 코드~~~");
+
+    useEffect(() => {
+        console.log("API 부르기~~");
+    }, []);
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("검색:", keyword);
+        }
+    }, [keyword]);
     return (
         <div>
-            <h1>어서오세용</h1>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here.."
+            ></input>
+            <h1>{value}</h1>
+            <button onClick={onClick}>꾹</button>
         </div>
     );
 }
