1595ed26c765dc22965befc1341be5b0e73997a7
miladiashe <dkdud0122@naver.com>
2022-01-21 18:57:15 +0900

    todo 리스트 제작

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 function App() {
-    return <div></div>;
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+
+        setToDo("");
+        setToDos((currentArray) => [toDo, ...toDos]);
+    };
+    console.log(toDos);
+    return (
+        <div>
+            <h1>MY TO DOS ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add TO DO</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
